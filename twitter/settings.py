53d74e0fbb650ce389732c06ee2124377afc8656
Liping7765 <liping.ma36@gmai.com>
2021-09-12 13:40:13 -0400

    6-tweet-model

--- twitter/settings.py
+++ twitter/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'tweets',
 ]
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
