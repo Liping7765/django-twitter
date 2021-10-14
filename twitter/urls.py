242c4bf42668a0a49ab77a1b9b51c132777b919f
Liping7765 <liping.ma36@gmai.com>
2021-10-13 20:42:32 -0400

    20-notifications-api-part-1

--- twitter/urls.py
+++ twitter/urls.py
@@ -16,21 +16,24 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from accounts.api import views
-from tweets.api.views import TweetViewSet
-from friendships.api.views import FriendshipViewSet
-from newsfeeds.api.views import NewsFeedViewSet
+
+from accounts.api.views import UserViewSet, AccountViewSet
 from comments.api.views import CommentViewSet
+from friendships.api.views import FriendshipViewSet
+from inbox.api.views import NotificationViewSet
 from likes.api.views import LikeViewSet
+from newsfeeds.api.views import NewsFeedViewSet
+from tweets.api.views import TweetViewSet
 
 router = routers.DefaultRouter()
-router.register(r'api/users', views.UserViewSet)
-router.register(r'api/accounts', views.AccountViewSet, basename='accounts')
+router.register(r'api/users', UserViewSet)
+router.register(r'api/accounts', AccountViewSet, basename='accounts')
 router.register(r'api/tweets', TweetViewSet, basename='tweets')
 router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
 router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeeds')
 router.register(r'api/comments', CommentViewSet, basename='comments')
 router.register(r'api/likes', LikeViewSet, basename='likes')
+router.register(r'api/notifications', NotificationViewSet, basename='notifications')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
