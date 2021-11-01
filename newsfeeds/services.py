e16351545ab8be369cef2dadd3b0cdf75f14dd1a
Liping7765 <liping.ma36@gmai.com>
2021-10-31 21:48:27 -0400

    34-cache-newsfeed-list-in-redis

--- newsfeeds/services.py
+++ newsfeeds/services.py
@@ -1,5 +1,7 @@
 from friendships.services import FriendshipService
 from newsfeeds.models import NewsFeed
+from twitter.cache import USER_NEWSFEEDS_PATTERN
+from utils.redis_helper import RedisHelper
 
 class NewsFeedService(object):
     @classmethod
@@ -10,4 +12,26 @@ class NewsFeedService(object):
         ]
 
         newsfeeds.append(NewsFeed(user=tweet.user,tweet=tweet))
-        return NewsFeed.objects.bulk_create(newsfeeds)
+        NewsFeed.objects.bulk_create(newsfeeds)
+
+        # bulk create doesn't provoke post_save's signal，manually push to cache 里
+        for newsfeed in newsfeeds:
+            cls.push_newsfeed_to_cache(newsfeed)
+
+    @classmethod
+    def get_cached_newsfeeds(cls, user_id):
+        queryset = NewsFeed.objects.filter(user_id=user_id).order_by('-created_at')
+        key = USER_NEWSFEEDS_PATTERN.format(user_id=user_id)
+        return RedisHelper.load_objects(key, queryset)
+
+    @classmethod
+    def push_newsfeed_to_cache(cls, newsfeed):
+        queryset = NewsFeed.objects.filter(user_id=newsfeed.user_id).order_by('-created_at')
+        key = USER_NEWSFEEDS_PATTERN.format(user_id=newsfeed.user_id)
+        RedisHelper.push_object(key, newsfeed, queryset)
+
+
+
+
+
+
