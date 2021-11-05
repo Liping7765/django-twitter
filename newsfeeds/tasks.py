1a522a3617bcc725fa496be39411d93967961ad7
Liping7765 <liping.ma36@gmai.com>
2021-11-04 22:00:56 -0400

    39-make-fanout-robust

--- newsfeeds/tasks.py
+++ newsfeeds/tasks.py
@@ -1,31 +1,45 @@
 from celery import shared_task
 from friendships.services import FriendshipService
+from newsfeeds.constants import FANOUT_BATCH_SIZE
 from newsfeeds.models import NewsFeed
-from tweets.models import Tweet
 from utils.time_constants import ONE_HOUR
 
 
-@shared_task(time_limit=ONE_HOUR)
-def fanout_newsfeeds_task(tweet_id):
-    # import 写在里面避免循环依赖
+@shared_task(routing_key='newsfeeds', time_limit=ONE_HOUR)
+def fanout_newsfeeds_batch_task(tweet_id, follower_ids):
+
     from newsfeeds.services import NewsFeedService
 
-    # DO NOT USE:
-    # N+1 QUERY
-    # for follower in FriendshipService.get_followers(tweet.user):
-    #     NewsFeed.objects.create(
-    #         user=follower,
-    #         tweet=tweet,
-    #     )
-    # CORRECT WAY:
-    tweet = Tweet.objects.get(id=tweet_id)
+    # DO NOT USE n+1 QUERY ->
+    # for follower_id in follower_ids:
+    #     NewsFeed.objects.create(user_id=follower_id, tweet_id=tweet_id)
+    # CORRECT WAY ->
     newsfeeds = [
-        NewsFeed(user=follower, tweet=tweet)
-        for follower in FriendshipService.get_followers(tweet.user)
+        NewsFeed(user_id=follower_id, tweet_id=tweet_id)
+        for follower_id in follower_ids
     ]
-    newsfeeds.append(NewsFeed(user=tweet.user, tweet=tweet))
     NewsFeed.objects.bulk_create(newsfeeds)
-
-    # BULK_CREATE does not signal POST_SAVE,so we manually push in CACHE
+    # Manually update CACHE due to BULK_CREATE do not invoke signals
     for newsfeed in newsfeeds:
         NewsFeedService.push_newsfeed_to_cache(newsfeed)
+
+    return "{} newsfeeds created".format(len(newsfeeds))
+
+
+@shared_task(routing_key='default', time_limit=ONE_HOUR)
+def fanout_newsfeeds_main_task(tweet_id, tweet_user_id):
+    # Ensure owner of the post sees it first
+    NewsFeed.objects.create(user_id=tweet_user_id, tweet_id=tweet_id)
+
+    # get all follower ids and break them into batches
+    follower_ids = FriendshipService.get_follower_ids(tweet_user_id)
+    index = 0
+    while index < len(follower_ids):
+        batch_ids = follower_ids[index: index + FANOUT_BATCH_SIZE]
+        fanout_newsfeeds_batch_task.delay(tweet_id, batch_ids)
+        index += FANOUT_BATCH_SIZE
+
+    return '{} newsfeeds going to fanout, {} batches created.'.format(
+        len(follower_ids),
+        (len(follower_ids) - 1) // FANOUT_BATCH_SIZE + 1,
+    )
